ff8be4b19013337dbb681c7bb6585396d9989bbc
camchu2001 <chuphuongcam@gmail.com>
2023-05-02 02:30:46 -0500

    feat: prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const operator = await prisma.operator.create({
+    data: {
+      firstName: "Alice",
+      lastName: "Hello",
+    },
+  });
+
+  const business = await prisma.business.create({
+    data: {
+      name: "DEF",
+      description: "ABCDE",
+      email: "somebody@gmail",
+      addressLine1: "hello",
+      addressLine2: "helloworld",
+      city: "Dallas",
+      state: "TX",
+      zip: "75080",
+    },
+  });
+
+  const op = await prisma.op.create({
+    data: {
+      operatorID: operator.id,
+      businessID: business.id,
+      title: "Backend Developer",
+      description: "Develop and maintain backend.",
+      pay: 25,
+      location: "123 Main St, Anytown USA",
+      startTime: "2023-05-02T10:11:46.518Z",
+      endTime: "2023-05-02T10:11:46.518Z",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
